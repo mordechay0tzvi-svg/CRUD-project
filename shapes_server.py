afbb90e56ad07b2378f988935915df6fb5844eb6
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-06-02 11:02:55 +0300

    need to figure out how to addparameters

--- shapes_server.py
+++ shapes_server.py
@@ -11,32 +11,10 @@ sm = ShapeManager()
 
 @app.get("/shapes")
 def get__shapes():
-    sm.get_all_shapes()
-
-
-@app.get("/shapes/{id}")
-def get_shape(id):
+    shape_list = []
     for shape in sm.get_all_shapes():
-        if shape.shape_id == id:
-            return shape.to_dict()
-
-
-@app.post("/shapes")
-def create(shape: dict = Body(...)):
-    sm.create_shape(shape)
-    return {"message": "created"}
-
-
-@app.put("/shapes/{id}")
-def replace(id):
-    sm.update_shape(id)
-    return {"message":f"{id} updated"}
-
-
-@app.delete("/shapes/{id}")
-def delete(id):
-    sm.delete_shape(id)
-    return {"message":f"{id} deleted"}
+        shape_list.append(shape.to_dict())
+    return shape_list
 
 
 @app.get("/shapes/total-area")
@@ -52,7 +30,32 @@ def count():
     return {"count":len(sm.get_all_shapes())}
 
 
+@app.get("/shapes/{id}")
+def get_shape(id:int):
+    for shape in sm.get_all_shapes():
+        if shape.shape_id == id:
+            return shape.to_dict()
 
+
+@app.post("/shapes")
+def create(shape: dict = Body(...)):
+    sm.create_shape(shape)
+    return {"message": "created"}
+
+
+@app.put("/shapes/{id}")
+def replace(id:int, new_data=Body(...)):
+    sm.update_shape(id, new_data)
+    return {"message":f"{id} updated"}
+
+
+@app.delete("/shapes/{id}")
+def delete(id:int):
+    sm.delete_shape(id)
+    return {"message":f"{id} deleted"}
+
+
+uvicorn.run(app, host="localhost", port="8888")
 
 
 
