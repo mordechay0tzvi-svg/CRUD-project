4bb0cee27ec86db14d8ec0542a7f2ca14e2926c9
Mordechai Bar-tal <mordechay0tzvi@gmail.com>
2026-06-03 15:07:37 +0300

    docstrings

--- shapes_server.py
+++ shapes_server.py
@@ -18,17 +18,20 @@ sm = ShapeManager()
 
 @app.get("/shapes")
 def get__shapes():
+    """returns the shape list from the instance or raises if no shape"""
     shape_list = []
-    for shape in sm.get_all_shapes():
-        shape_list.append(shape.to_dict())
     shapes = sm.get_all_shapes()
     if not shapes:
-        raise HTTPException(status_code=404, detail="no shapes yet")
+        raise HTTPException(status_code=404, detail="no shapes")
+    for shape in sm.get_all_shapes():
+        shape_list.append(shape.to_dict())
     return shape_list
+
 
 
 @app.get("/shapes/total-area")
 def total_area():
+    """returns the combiend area of all the shapes or raises an error if no shapes"""
     if not sm.get_all_shapes():
         raise HTTPException(status_code=404, detail="no shapes yet")
     total = 0 
@@ -38,6 +41,7 @@ def total_area():
 
 @app.get("/shapes/count")
 def count():
+    """returns how many shapes are"""
     if not sm.get_all_shapes():
         raise HTTPException(status_code=404, detail="no shapes yet")
     return {"count":len(sm.get_all_shapes())}
@@ -45,6 +49,7 @@ def count():
 
 @app.post("/shapes")
 def create(shape_data: dict = Body(...)): 
+    """adds a new shape and raises error if shape type is not good"""
     shape_type = shape_data.get("type")
     new_id = sm.get_id()
     if shape_type == "circle":
@@ -57,11 +62,12 @@ def create(shape_data: dict = Body(...)):
         raise HTTPException(status_code=400, detail="this shape type is not valid")
     sm.create_shape(shape_obj)
     return {"message": "created"}
-    
+
 
 
 @app.put("/shapes/{id}")
 def replace(id: int, new_data: dict = Body(...)):
+    """updates shape attributes or error if id is wrong"""
     shape_type = sm.find_type(id)
     if shape_type is None:
         raise HTTPException(status_code=400, detail="wrong id")
@@ -78,6 +84,7 @@ def replace(id: int, new_data: dict = Body(...)):
 
 @app.get("/shapes/{id}")
 def get_shape(id:int):
+    """returns a shape by it's id or error if id is wrong"""
     for shape in sm.get_all_shapes():
         if shape.shape_id == id:
             return shape.to_dict()
@@ -86,6 +93,7 @@ def get_shape(id:int):
 
 @app.delete("/shapes/{id}")
 def delete(id:int):
+    """deletes shape by it's id or error if id not exists"""
     try:
         sm.delete_shape(id)
         return {"message":f"{id} deleted"}
@@ -94,6 +102,7 @@ def delete(id:int):
 
 @app.get("/shapes/types/{type}")
 def type_filter(type:str):
+    """return all shape from certain type and error if type not valid"""
     filterd = []
     for shape in sm.get_all_shapes():
         if shape.type == type:
